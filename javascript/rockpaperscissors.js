8172815e6c75e761071e1891448087e2b33ac341
Abraham <hambenski@gmail.com>
2021-12-03 16:33:54 -0600

    - Removed old 5-round game functionality. - New UI consisting of title, round result, selection buttons, and score tracking.

--- javascript/rockpaperscissors.js
+++ javascript/rockpaperscissors.js
@@ -1,36 +1,9 @@
 const VALID_HANDS = ['rock', 'paper', 'scissors'];
-const rounds = roundCount();
+let currentResult = '';
+let playerScore = 0;
+let computerScore = 0;
 
-if (rounds === null) {
-    alert("Game ended.")
-}   else {
-    game();
-}
-
-// functions start here
-
-//  Prompt player for amount of rounds.
-function roundCount() {
-    let count = "";
-
-    //  Verify input is a valid number of playable rounds.
-    while (isNaN(count) || count < 1) {
-        count = prompt("How many rounds of Rock Paper Scissors would you like to play? (ex: 3) ");
-
-        //  Stop if user Cancels.
-        if (count === null) {
-            break;
-        }   else {
-
-            // Verify user input is a valid number. If not, inform them.
-            count = parseInt(count);
-            if (isNaN(count) || count < 1) {
-                alert("Please input an integer of 1 or greater.");
-            }
-        }
-    }
-    return count;
-}
+//  RPS functionality starts here
 
 //  Prompt player for Rock, Paper, or Scissors.
 function playerPlay() {
@@ -64,29 +37,81 @@ function computerPlay() {
 
 //  Plays a single round of RPS, and returns a string that declares the winner. (ex: "You Lose! Paper beats Rock")
 function singleRound(playerSelection, computerSelection) {
-    if (VALID_HANDS.indexOf(playerSelection) > VALID_HANDS.indexOf(computerSelection)) {
-        return `Your ${playerSelection} ~VS~ my ${computerSelection}! You win!`;
-    }   else if (VALID_HANDS.indexOf(playerSelection) < VALID_HANDS.indexOf(computerSelection)) {
-        return `Your ${playerSelection} ~VS~ my ${computerSelection}! You lose!`;
-    }   else {
+    if (playerSelection == computerSelection) {
         return `Your ${playerSelection} ~VS~ my ${computerSelection}. This one\'s a draw.`;
-    }
-}
-
-//  Loops individual round of RPS to play a game of user-specified number of rounds.
-function game() {
-    let player = "";
-
-    for (let i = 1; i <= rounds; i++) {
-        //  End if player Cancels.
-        player = playerPlay();
-        if (player === null) {
-            alert("Game ended.");
-            break;
+    }   else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            computerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You lose!`;
         }   else {
-            //  Display the results of each round.
-            alert(`Round ${i}: ${singleRound(player, computerPlay())}`);
-            console.log(`Round ${i}: ${singleRound(player, computerPlay())}`);
+            playerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You win!`;
+        }
+    }   else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            computerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You lose!`;
+        }   else {
+            playerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You win!`;
+        }
+    }   else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            computerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You lose!`;
+        }   else {
+            playerScore++
+            return `Your ${playerSelection} ~VS~ my ${computerSelection}! You win!`;
         }
     }
 }
+
+//  UI features start here
+
+//  Buttons used to select player's hand.
+const buttons = Array.from(document.querySelectorAll('button'));
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        currentResult = singleRound(button.dataset.hand, computerPlay())
+        console.log(currentResult);
+    });
+});
+
+//  Update the DOM with result of last-played round.
+const roundContainer = document.querySelector('.round-result');
+const roundResult = document.createElement('div');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        roundResult.textContent = currentResult;
+        roundContainer.appendChild(roundResult);
+    });
+});
+
+//  Display the running score for the match.
+const scoreContainer = document.querySelector('.overall-score');
+const overallScore = document.createElement('div');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        overallScore.textContent = `Your Wins: ${playerScore} ~ Computer Wins: ${computerScore}`;
+        scoreContainer.appendChild(overallScore);
+    });
+});
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (playerScore == 5) {
+            overallScore.textContent = "Game Over! You win!";
+            scoreContainer.appendChild(overallScore);
+            playerScore = 0;
+            computerScore = 0;
+        }   else if (computerScore == 5) {
+            overallScore.textContent = "Game Over! You lose!";
+            scoreContainer.appendChild(overallScore);
+            playerScore = 0;
+            computerScore = 0;
+        }
+    });
+});
